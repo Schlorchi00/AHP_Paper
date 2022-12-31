0c5903aea856f8c76d3c8e001dd0aae2bbdec791
Nico Mandel <mandel.nicolas@gmail.com>
2022-12-31 11:14:27 +0100

    fixed row index for reading in files

--- src/ahp/utils.py
+++ src/ahp/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 def read_excel(fpath : str) -> list:
     # multiple workbooks
     with open(fpath, 'rb') as f:
-        df = pd.read_excel(f, sheet_name=None)
+        df = pd.read_excel(f, sheet_name=None, index_col=0)
         # df_list = [df.parse(sheet) for sheet in df.sheet_names]
     # return df_list
     return df
