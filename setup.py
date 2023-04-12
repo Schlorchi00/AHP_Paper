7716561db80a8c0e930ae3c019772d6b036db5ee
Schlorchi00 <niko.nagenast@gmail.com>
2023-04-12 18:27:23 +0200

    Change setup.py config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ahp',
-    verion='0.1.0',
+    version='0.1.0',
     packages=find_packages(include=['src']),
-    package_dir={'': 'src'}
+    package_dir={'src': 'src'}
 )
