f188549b1b4136a4d1a74afffbd63603dad9350a
Schlorchi00 <niko.nagenast@gmail.com>
2023-05-04 10:08:42 +0200

    Insert workbook preperation

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,4 +1,5 @@
 import os.path
+import openpyxl
 from ahp.utils import *
 
 
@@ -48,7 +49,7 @@ from ahp.utils import *
                             * [ ] Check which Table (Performance, Process, Ecology, Cost)
                             * [ ] Extract params (other module)
                             + [ ] Store them in dictionary
-                                * load .xlsx - file
+                                * load  .xlsx - file
                                 
             * [ ] save total cost .xlsx - file
                 * [ ] horizontal stack of alternatives 
@@ -66,14 +67,89 @@ from ahp.utils import *
 def print_something():
     print("Use this structure to develop functions. Only move to utils when done")
 
-if __name__=="__main__":
-    print_something()
-    ppath = os.path.join(os.path.abspath(os.path.dirname(__file__)))
-    datadir = os.path.join(ppath, '..', 'data', 'ecology')
+def read_domain(subdomain, filename, data = 'data'):
+    """
+    loads excel files and creates workbook
+    :param subdomain: string of the name regarding the subdomain
+    :param filename: string of filename.xlsx
+    :param data: string of the foldername
+    :return: workbook
+    """
 
-    df_path = os.path.join(datadir, 'LCA_Endpoints_PLA_Coins.xlsx' )
-    df = read_excel(df_path, series=False)
-    print(df)
+    ppath = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+    datadir = os.path.join(ppath, '..', data, subdomain)
+    df_path = os.path.join(datadir,filename)
+    wb = openpyxl.load_workbook(df_path)
+
+    return wb
+
+#create data frames
+
+def create_df(wb, sheet1 = 'Sheet1', sheet2 = None, sheet3 = None):
+    """
+    modifies a workbook into a suitable dataframe
+    :param wb: workbook
+    :return: dataframe
+    """
+    if sheet2 == None  and sheet3 == None:
+        sheet_1 = wb[sheet1]
+        df = pd.DataFrame(sheet_1.values)
+        df = df.rename(columns=df.iloc[1])
+        df = df.iloc[2:,:].reset_index(drop=True)
+
+        return df
+
+    if sheet3 == None:
+
+        sheet_1 = wb[sheet1]
+        df = pd.DataFrame(sheet_1.values)
+        df = df.rename(columns=df.iloc[1])
+        df_1 = df.iloc[2:,:].reset_index(drop=True)
+
+        sheet_2 = wb[sheet2]
+        df = pd.DataFrame(sheet_2.values)
+        df = df.rename(columns=df.iloc[1])
+        df_2 = df.iloc[2:, :].reset_index(drop=True)
+
+        return df_1, df_2
+
+    else:
+        sheet_1 = wb[sheet1]
+        df = pd.DataFrame(sheet_1.values)
+        df = df.rename(columns=df.iloc[1])
+        df_1 = df.iloc[2:, :].reset_index(drop=True)
+
+        sheet_2 = wb[sheet2]
+        df = pd.DataFrame(sheet_2.values)
+        df = df.rename(columns=df.iloc[1])
+        df_2 = df.iloc[2:, :].reset_index(drop=True)
+
+        sheet_3 = wb[sheet3]
+        df = pd.DataFrame(sheet_3.values)
+        df = df.rename(columns=df.iloc[1])
+        df_3 = df.iloc[2:, :].reset_index(drop=True)
+
+        return df_1, df_2, df_3
+
+
+
+
+if __name__=="__main__":
+
+    print_something()
+
+    #read files TODO: Check correctness regarding amount of leaf nodes
+
+    wb_eco = read_domain('ecology', 'LCA_Endpoints_PLA_Coins.xlsx')
+    wb_mech_90 = read_domain('performance', 'Mechanical_Params_90_degrees.xlsx')
+    wb_mech_0 = read_domain('performance', 'Mechanical_Params_0_degrees.xlsx')
+    wb_process_fila = read_domain('process', 'FILA_CHAR.xlsx')
+    wb_process_mvr = read_domain('process', 'MFI_Polymers.xlsx')
+    wb_cost = read_domain('cost', 'cost_polymers.xlsx')
+
+    #create df
+    df_mech_90 = create_df(wb_mech_90, sheet1 = 'E_modulus', sheet2='tensile strain', sheet3='tensile stress')
+
 
     # preprocess here
 
