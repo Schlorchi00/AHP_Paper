704c7b9398eff4598e0cb4c4f3dba0bbef1ba8a0
Nico Mandel <mandel.nicolas@gmail.com>
2023-09-06 12:54:20 +0200

    added weighting of cost per gramm script

--- scripts/cost_combination.py
+++ scripts/cost_combination.py
@@ -35,12 +35,16 @@ def parse_args():
     parser.add_argument("-i", "--input", type=str,  required=True, action="append", help="Location of the cost excel file. Accepts multiple arguments")
     parser.add_argument("-n", "--name", action="append", help="Name of a material - will be used in output file. Caution - keep order!")
     parser.add_argument("-v", "--value", action="append", help="Value of material - will be used in output file. Caution - keep order!")
+    parser.add_argument("-wn", "--weightnames", action="append", help="Percentage weight of the material for the named materials. Has to sum to 1 with weightinputs! Caution - keep order!")
+    parser.add_argument("-wi", "--weightinputs", action="append", help="Percentage weight of the material for the input files. Has to sum to 1 with weightnames! Caution - keep order, only f input files!")
     parser.add_argument("-t", "--time", help="Whether to use time or energy for operational cost calculation. Defaults to energy.", action="store_true")
     parser.add_argument("-o", "--output", type=str, default=None, help="Output location of the file. If None given, will write to terminal")
     parser.add_argument("-s", "--scale", type=int, required=True, action="append", help="Weight of recycling mass in g - to rescale to euros per g")
     args = parser.parse_args()
     return vars(args)
 
+def _to_float(l : list) -> list[float]:
+    return [float(v) for v in l]
 
 # call main function - safeguard for external includes
 if __name__ == "__main__":
@@ -51,15 +55,27 @@ if __name__ == "__main__":
     ns = args["name"]
     # print(ns)
     vs = args["value"]
-    ws = args["Weights"]
+    vs = _to_float(vs)
+    wsn = args["weightnames"]
+    wsn = _to_float(wsn)
+    wsi = args["weightinputs"]
+    wsi = _to_float(wsi)
+    assert abs(sum(wsn + wsi) - 1.) <= 0.001, "Weights do not sum to 1, sum to : {}. Double Check".format(sum(wsn + wsi))
+    assert len(wsn) == len(ns) , "Weights are {} items, names are: {}. Have to be the same length".format(len(wsn), len(ns))
+    assert len(wsi) == len(path_costtable), "Weights are {} items, input files are: {}. Have to be the same length".format(len(wsn), len(path_costtable))
     # print(vs)
+    # df = pd.DataFrame(columns=["cost_per_gram" ,"weight", "cost_weighted"])
+    d = {}
     if ns:
         assert len(ns) == len(vs), "Not the correct number of values provided for names. Check if number coincides"
-        ns_vs = {ns[i] : float(vs[i]) for i in range(len(ns))}
-    else:
-        ns_vs = {}
-
-    # scaling values
+        for i, n in enumerate(ns):
+            sd = {}
+            sd["cost_per_gram"] = vs[i]
+            sd["weight"] = wsn[i]
+            sd["cost_weighted"] = wsn[i] * vs[i]
+            d[n] = sd
+        # ns_vs = {ns[i] : float(vs[i]) * float(wsn[i]) for i in range(len(ns))}
+    # # scaling values
     scales = args["scale"]
     assert len(scales) == len(path_costtable), "Not the correct number of scales given for input files. Check that a scale is given for each input file"
 
@@ -69,7 +85,7 @@ if __name__ == "__main__":
             warnings.filterwarnings('ignore')
             basename = os.path.basename(pth).split(".")[0]
             wbs = read_sheets(pth)
-
+            sd = {}
             # Calculate the total cost
             tot_cost = total_cost(wbs, args["time"])
             tot_cost_g = tot_cost / scales[i]
@@ -77,11 +93,13 @@ if __name__ == "__main__":
                 basename, tot_cost, tot_cost_g
             ))
             # add it to the dictionary
-            ns_vs[basename] = tot_cost_g
+            sd["cost_per_gram"] = tot_cost_g
+            sd["weight"] = wsi[i]
+            sd["cost_weighted"] = wsi[i] * tot_cost_g
+            d[basename] = sd
+            # ns_vs[basename] = tot_cost_g * wsi[i]
 
-    df = pd.Series(ns_vs).to_frame().T
-    df.index = ["total_cost_filament"]
-
+    df = pd.DataFrame.from_dict(d, orient="index")
     if args["output"]:
         df_scaling = pd.DataFrame(data=pd.NA, index=df.index, columns=["Min", "Max", "Inversion"])
         logging.warning("Scaling sheet appended. Please correct values before using for preprocessing!")
@@ -90,8 +108,3 @@ if __name__ == "__main__":
             df_scaling.to_excel(writer, sheet_name="Scaling")
     else:
         print(df)
-
-#TODO: 3) modify input cost table?
-#TODO: 4) how do we operate with two material combinations (PP+Support), (PLA+Support)?
-
-
