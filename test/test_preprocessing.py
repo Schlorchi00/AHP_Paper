f51f056302cc28b5025d1778634a2c2e4f7c5fcb
Nico Mandel <mandel.nicolas@gmail.com>
2023-09-14 12:10:31 +0200

    added basic tests for directory in test itself

--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -1,1 +1,8 @@
 import pytest
+import os.path
+
+DATADIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'preprocessing'))
+
+def test_dir_loc():
+    assert os.path.isdir(DATADIR)
+    print("Datadir: {} exists".format(DATADIR))
