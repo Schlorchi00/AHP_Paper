28366d9f5c1eacb7c3c387ddca860b069e9e9c8a
Nico Mandel <mandel.nicolas@gmail.com>
2024-01-25 09:32:39 +0100

    added usage examples to test_script

--- scripts/test_ahp.py
+++ scripts/test_ahp.py
@@ -8,24 +8,40 @@ def parse_args():
     args = parser.parse_args()
     return vars(args)
 
-if __name__=="__main__":
-    logging.getLogger().setLevel(logging.DEBUG)
-    logging.getLogger('matplotlib.font_manager').disabled = True
+def plot_complete_tree(tree : TreeNode) -> None:
+    """
+        Example function to plot a completely calculated tree. Plotting Tree with lambdas and values
+    """
+    treepl = tree.plot_tree()
+    treepl.view("testview")
 
+if __name__=="__main__":
+    # Uncomment for debugging purposes
+    # logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger('matplotlib.font_manager').disabled = True
     args = parse_args()
+
+    # 1. Reading in the tree
     ahp = TreeNode.tree_from_directory(args["input"])
 
+    # 2. Calculating the tree
     ahp.prepare_tree()
     ahp.check_integrity()
     ahp.calculate_tree()
     print("Final Results:\n{}".format(ahp.calculate_tree()))
-    treepl = ahp.plot_tree()
-    treepl.view("testview")
+    
+    # 3. Visualising results
+    # a. plotting lambdas and values as tree
+    plot_complete_tree(ahp)
 
+    # b. Plotting value bar charts - uncomment for use
+    # ahp.plot_values()
     # for c in ahp.children:
     #     c.plot_values()
-    # ahp.plot_values()
+    
+    # c. Plotting weights tree - uncomment for use
     # ahp.plot_weights_tree()
 
+    # 4. Storing results - uncomment for use
     # ahp.save_values("test_output.xlsx")
     # ahp.save_lambda("test_lambda.xlsx")
